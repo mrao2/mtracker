d6c8c9006ae2b71d75f824d25684291968b164a1
Mohith Rao <mohith.rao96@gmail.com>
2018-04-04 20:24:37 -0500

    minor bug fixes

--- src/components/table.js
+++ src/components/table.js
@@ -16,7 +16,7 @@ const selectRow = {
 export default class Table extends React.Component {
   render() {
     return (
-      <BootstrapTable data={ money } cellEdit={ cellEditProp } insertRow={ true } deleteRow={ true } selectRow={ selectRow } exportCSV>
+      <BootstrapTable data={ money } cellEdit={ cellEditProp } insertRow={ true } deleteRow={ true } selectRow={ selectRow } exportCSV={ true }>
         <TableHeaderColumn dataField='id' isKey={ true }>Entry Id</TableHeaderColumn>
         <TableHeaderColumn dataField='name' editable={ { type: 'textarea' } }>Item Name</TableHeaderColumn>
         <TableHeaderColumn dataField='type' editable={ { type: 'select', options: { values: purchaseTypes } } }>Item Type</TableHeaderColumn>
